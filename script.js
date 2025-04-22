347eb1642b7de376af97246aa10e6c2670806b0a
Bogdan5ik <baggi.juk@mail.ru>
2025-04-22 10:32:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+let count = parseInt(localStorage.getItem("clickCount")) || 0;
+let multiplier = parseInt(localStorage.getItem("clickMultiplier")) || 1;
+let upgradeCost = parseInt(localStorage.getItem("upgradeCost")) || 100;
+
+
+const clickButton = document.querySelector(".img-1");
+const sound = document.querySelector(".audio");
+const scoreDisplay = document.querySelector(".p-3");
+const upgradeButton = document.getElementById("upgrade-btn");
+
+
+scoreDisplay.textContent = count;
+upgradeButton.textContent = `Улучшить клики (Цена: ${upgradeCost})`;
+
+
+clickButton.addEventListener("click", () => {
+  count += multiplier;
+  scoreDisplay.textContent = count;
+  localStorage.setItem("clickCount", count);
+
+
+  sound.currentTime = 0;
+  sound.play();
+});
+
+
+
+upgradeButton.addEventListener("click", () => {
+  if (count >= upgradeCost) {
+    count -= upgradeCost;
+    multiplier += 1;
+    upgradeCost *= 5;
+
+    scoreDisplay.textContent = count;
+    upgradeButton.textContent = `Улучшить клики (Цена: ${upgradeCost})`;
+
+    localStorage.setItem("clickCount", count);
+    localStorage.setItem("clickMultiplier", multiplier);
+    localStorage.setItem("upgradeCost", upgradeCost);
+  } else {
+    alert("Не хватает очков!");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
