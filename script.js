64a0adb1a60807a638fdb4749b6830d331931e87
Bogdan5ik <baggi.juk@mail.ru>
2025-04-23 18:03:06 +0600

    Add files via upload
    Global

--- script.js
+++ script.js
@@ -2,27 +2,45 @@ let count = parseInt(localStorage.getItem("clickCount")) || 0;
 let multiplier = parseInt(localStorage.getItem("clickMultiplier")) || 1;
 let upgradeCost = parseInt(localStorage.getItem("upgradeCost")) || 100;
 
+let unlockedSkins = JSON.parse(localStorage.getItem("unlockedSkins")) || {
+  skin1: false,
+  skin2: false,
+  skin3: false
+};
 
 const clickButton = document.querySelector(".img-1");
 const sound = document.querySelector(".audio");
 const scoreDisplay = document.querySelector(".p-3");
 const upgradeButton = document.getElementById("upgrade-btn");
 
-
 scoreDisplay.textContent = count;
 upgradeButton.textContent = `Улучшить клики (Цена: ${upgradeCost})`;
+
+function checkSkins() {
+  if (count >= 10000 && !unlockedSkins.skin1) {
+    unlockedSkins.skin1 = true;
+    alert("Вы открыли новый скин!");
+  }
+  if (count >= 50000 && !unlockedSkins.skin2) {
+    unlockedSkins.skin2 = true;
+    alert("Вы открыли новый скин!");
+  }
+  if (count >= 100000 && !unlockedSkins.skin3) {
+    unlockedSkins.skin3 = true;
+    alert("Вы открыли новый скин!");
+  }
+
+  localStorage.setItem("unlockedSkins", JSON.stringify(unlockedSkins));
+}
 
 clickButton.addEventListener("click", () => {
   count += multiplier;
   scoreDisplay.textContent = count;
   localStorage.setItem("clickCount", count);
-
-
+  checkSkins();
   sound.currentTime = 0;
   sound.play();
 });
-
-
 
 upgradeButton.addEventListener("click", () => {
   if (count >= upgradeCost) {
@@ -44,17 +62,14 @@ upgradeButton.addEventListener("click", () => {
 document.body.style.zoom = "1";
 
 
+const activeSkin = localStorage.getItem("activeSkin") || "default";
+const clickButton1 = document.querySelector(".img-1");
 
+const skinMap = {
+  "default": "img/pngwing.com.png",
+  "EMG-5": "img/img1.png",
+  "EMG-6": "img/img2.png",
+  "EMG-7": "img/img3.png"
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
+clickButton1.src = skinMap[activeSkin] || skinMap["default"];
--- two/script.js
+++ two/script.js
@@ -1,3 +1,35 @@
 function goback() {
     window.history.back()
 }
+
+window.onload = function () {
+    const skins = JSON.parse(localStorage.getItem("unlockedSkins")) || {};
+  
+    if (skins.skin1) {
+      document.querySelector(".img-5").style.display = "flex";
+      document.querySelector(".img-2").style.display = "none";
+    }
+  
+    if (skins.skin2) {
+      document.querySelector(".img-6").style.display = "flex";
+      document.querySelector(".img-3").style.display = "none";
+    }
+  
+    if (skins.skin3) {
+      document.querySelector(".img-7").style.display = "flex";
+      document.querySelector(".img-4").style.display = "none";
+    }
+  };
+  
+
+
+  function selectSkin(skinName) {
+    localStorage.setItem("activeSkin", skinName);
+    alert("Вы надели скин!");
+  }
+  
+  document.querySelector(".img-1").addEventListener("click", () => selectSkin("default"));
+  document.querySelector(".img-5").addEventListener("click", () => selectSkin("EMG-5"));
+  document.querySelector(".img-6").addEventListener("click", () => selectSkin("EMG-6"));
+  document.querySelector(".img-7").addEventListener("click", () => selectSkin("EMG-7"));
+  
