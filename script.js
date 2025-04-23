300df7a92f4ddfe65900c158ad9a7d046617988c
Bogdan5ik <baggi.juk@mail.ru>
2025-04-23 15:38:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let count = parseInt(localStorage.getItem("clickCount")) || 0;
 let multiplier = parseInt(localStorage.getItem("clickMultiplier")) || 1;
 let upgradeCost = parseInt(localStorage.getItem("upgradeCost")) || 100;
 
+
 const clickButton = document.querySelector(".img-1");
 const sound = document.querySelector(".audio");
 const scoreDisplay = document.querySelector(".p-3");
 const upgradeButton = document.getElementById("upgrade-btn");
+
 
 scoreDisplay.textContent = count;
 upgradeButton.textContent = `Улучшить клики (Цена: ${upgradeCost})`;
@@ -15,9 +17,12 @@ clickButton.addEventListener("click", () => {
   scoreDisplay.textContent = count;
   localStorage.setItem("clickCount", count);
 
+
   sound.currentTime = 0;
   sound.play();
 });
+
+
 
 upgradeButton.addEventListener("click", () => {
   if (count >= upgradeCost) {
@@ -38,37 +43,18 @@ upgradeButton.addEventListener("click", () => {
 
 document.body.style.zoom = "1";
 
-// Добавляем функциональность удержания на 5 секунд
-let holdTimer;
 
-function addBillionPoints() {
-  count += 1000000000;
-  scoreDisplay.textContent = count;
-  localStorage.setItem("clickCount", count);
-}
 
-// Для настольных устройств
-clickButton.addEventListener("mousedown", () => {
-  holdTimer = setTimeout(addBillionPoints, 5000);
-});
 
-clickButton.addEventListener("mouseup", () => {
-  clearTimeout(holdTimer);
-});
 
-clickButton.addEventListener("mouseleave", () => {
-  clearTimeout(holdTimer);
-});
 
-// Для мобильных устройств
-clickButton.addEventListener("touchstart", () => {
-  holdTimer = setTimeout(addBillionPoints, 5000);
-});
 
-clickButton.addEventListener("touchend", () => {
-  clearTimeout(holdTimer);
-});
 
-clickButton.addEventListener("touchcancel", () => {
-  clearTimeout(holdTimer);
-});
+
+
+
+
+
+
+
+
