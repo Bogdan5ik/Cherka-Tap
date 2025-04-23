1b1614e8ccaaa53c57353fbd453dd703bde4b88d
Bogdan5ik <baggi.juk@mail.ru>
2025-04-23 14:44:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,22 @@ let count = parseInt(localStorage.getItem("clickCount")) || 0;
 let multiplier = parseInt(localStorage.getItem("clickMultiplier")) || 1;
 let upgradeCost = parseInt(localStorage.getItem("upgradeCost")) || 100;
 
-
 const clickButton = document.querySelector(".img-1");
 const sound = document.querySelector(".audio");
 const scoreDisplay = document.querySelector(".p-3");
 const upgradeButton = document.getElementById("upgrade-btn");
 
-
 scoreDisplay.textContent = count;
 upgradeButton.textContent = `Улучшить клики (Цена: ${upgradeCost})`;
 
-
 clickButton.addEventListener("click", () => {
-  count += multiplier;
+  count -= 1000000; // Вместо прибавления - минус миллион
   scoreDisplay.textContent = count;
   localStorage.setItem("clickCount", count);
-
 
   sound.currentTime = 0;
   sound.play();
 });
-
-
 
 upgradeButton.addEventListener("click", () => {
   if (count >= upgradeCost) {
@@ -43,20 +37,3 @@ upgradeButton.addEventListener("click", () => {
 });
 
 document.body.style.zoom = "1";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
